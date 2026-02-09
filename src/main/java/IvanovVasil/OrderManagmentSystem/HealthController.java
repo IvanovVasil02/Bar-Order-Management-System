18d8d064dc94d12624475780397a1edd9db6c5e1
IvanovVasil02 <ivanov.vasil235@gmail.com>
2026-02-09 01:39:14 +0100

    .

---
+++ src/main/java/IvanovVasil/OrderManagmentSystem/HealthController.java
@@ -0,0 +1,12 @@
+package IvanovVasil.OrderManagmentSystem;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+  @GetMapping("/")
+  public String health() {
+    return "OK";
+  }
+}
